bc4c8aa4f7fbbae82e787b8da4b161b24d828fa1
Rene Dohmen <acidjunk@gmail.com>
2018-03-26 09:51:54 +0200

    Increased code quality and made the test python27 compatible

--- tests/integration/test_ims.py
+++ tests/integration/test_ims.py
@@ -5,9 +5,9 @@ import requests_mock
 import zeep
 
 
-def read_file(file_name, folder='wsdl_ims'):
+def read_file(file_name, folder="wsdl_ims"):
     file = os.path.join(os.path.dirname(os.path.realpath(__file__)), folder, file_name)
-    with open(file, encoding='UTF-8') as f:
+    with open(file, encoding="UTF-8") as f:
         return f.read()
 
 
@@ -15,31 +15,31 @@ def test_find_product(ims_login_token):
     with requests_mock.mock() as m:
         m.get("http://example.com/inventory?wsdl", text=read_file("inventory.wsdl"))
         for i in range(33):
-            m.get(f"http://example.com/Inventory?xsd=xsd{i}",
-                  text=read_file(f"Inventory?xsd=xsd{i}"))
+            m.get("http://example.com/Inventory?xsd=xsd{}".format(i),
+                  text=read_file("Inventory?xsd=xsd{}".format(i)))
         m.post("http://example.com/Inventory/iInventoryHttp",
-               text=read_file("find_product_by_name_response.xml", 'mock_ims'))
+               text=read_file("find_product_by_name_response.xml", "mock_ims"))
         # set strict to True -> then data will be available in _raw_elements
-        client = zeep.Client('http://example.com/inventory?wsdl', strict=True)
+        client = zeep.Client("http://example.com/inventory?wsdl", strict=True)
         filter_fields = [{"FilterField": {"Name": "Name", "SelectedOperator": "OperationEquals", "Value": "LP"}}]
-        filter = {"Filters": filter_fields}
+        ims_filter = {"Filters": filter_fields}
         pager = {"StartElement": 0, "Descending": False, "NumberOfElements": 10,
                  "OrderByProperty": None}
-        assert client.service.GetAllProductsFiltered(pager=pager, filter=filter, sessionToken=ims_login_token)
+        assert client.service.GetAllProductsFiltered(pager=pager, filter=ims_filter, sessionToken=ims_login_token)
 
 
 def test_find_customer(ims_login_token):
     with requests_mock.mock() as m:
         m.get("http://example.com/inventory?wsdl", text=read_file("inventory.wsdl"))
         for i in range(33):
-            m.get(f"http://example.com/Inventory?xsd=xsd{i}",
-                  text=read_file(f"Inventory?xsd=xsd{i}"))
+            m.get("http://example.com/Inventory?xsd=xsd{}".format(i),
+                  text=read_file("Inventory?xsd=xsd{}".format(i)))
         m.post("http://example.com/Inventory/iInventoryHttp",
-               text=read_file("find_customer_by_name_response.xml", 'mock_ims'))
+               text=read_file("find_customer_by_name_response.xml", "mock_ims"))
         # set strict to True -> then data will be available in _raw_elements
-        client = zeep.Client('http://example.com/inventory?wsdl', strict=True)
+        client = zeep.Client("http://example.com/inventory?wsdl", strict=True)
         filter_fields = [{"FilterField": {"Name": "Name", "SelectedOperator": "OperationEquals", "Value": "SURFNET"}}]
-        filter = {"Filters": filter_fields}
+        ims_filter = {"Filters": filter_fields}
         pager = {"StartElement": 0, "Descending": False, "NumberOfElements": 10,
                  "OrderByProperty": None}
-        assert client.service.GetAllCustomersFiltered(pager=pager, filter=filter, sessionToken=ims_login_token)
+        assert client.service.GetAllCustomersFiltered(pager=pager, filter=ims_filter, sessionToken=ims_login_token)
