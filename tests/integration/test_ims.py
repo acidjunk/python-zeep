7db0e215604166ed267c6796e056f8cee5d45ddc
Rene Dohmen <acidjunk@gmail.com>
2018-03-27 14:04:19 +0200

    Added the test which contains the fail again

--- tests/integration/test_ims.py
+++ tests/integration/test_ims.py
@@ -11,21 +11,21 @@ def read_file(file_name, folder="wsdl_ims"):
         return f.read()
 
 
-# def test_find_product(ims_login_token):
-#     with requests_mock.mock() as m:
-#         m.get("http://example.com/inventory?wsdl", text=read_file("inventory.wsdl"))
-#         for i in range(33):
-#             m.get("http://example.com/Inventory?xsd=xsd{}".format(i),
-#                   text=read_file("inventory_xsd{}.xsd".format(i)))
-#         m.post("http://example.com/Inventory/iInventoryHttp",
-#                text=read_file("find_product_by_name_response.xml", "mock_ims"))
-#         # set strict to True -> then data will be available in _raw_elements
-#         client = zeep.Client("http://example.com/inventory?wsdl", strict=True)
-#         filter_fields = [{"FilterField": {"Name": "Name", "SelectedOperator": "OperationEquals", "Value": "LP"}}]
-#         ims_filter = {"Filters": filter_fields}
-#         pager = {"StartElement": 0, "Descending": False, "NumberOfElements": 10,
-#                  "OrderByProperty": None}
-#         assert client.service.GetAllProductsFiltered(pager=pager, filter=ims_filter, sessionToken=ims_login_token)
+def test_find_product(ims_login_token):
+    with requests_mock.mock() as m:
+        m.get("http://example.com/inventory?wsdl", text=read_file("inventory.wsdl"))
+        for i in range(33):
+            m.get("http://example.com/Inventory?xsd=xsd{}".format(i),
+                  text=read_file("inventory_xsd{}.xsd".format(i)))
+        m.post("http://example.com/Inventory/iInventoryHttp",
+               text=read_file("find_product_by_name_response.xml", "mock_ims"))
+        # set strict to True -> then data will be available in _raw_elements
+        client = zeep.Client("http://example.com/inventory?wsdl", strict=True)
+        filter_fields = [{"FilterField": {"Name": "Name", "SelectedOperator": "OperationEquals", "Value": "LP"}}]
+        ims_filter = {"Filters": filter_fields}
+        pager = {"StartElement": 0, "Descending": False, "NumberOfElements": 10,
+                 "OrderByProperty": None}
+        assert client.service.GetAllProductsFiltered(pager=pager, filter=ims_filter, sessionToken=ims_login_token)
 
 
 def test_find_customer(ims_login_token):
